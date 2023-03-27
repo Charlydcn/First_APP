b099ed7b250b52962b4e5c585ed173842488102a
Charlydcn <charly.ducournau@outlook.fr>
2023-03-27 11:21:37 +0200

    created form to add a product

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="">
+        <fieldset>
+            <legend>Ajout d'un produit</legend>
+            <p>
+                <label for="product_name">Nom du produit :</label>
+                <input type="text" name="product_name" id="product_name">
+                <br>                
+            </p>
+            <p>
+                <label for="product_price">Prix du produit :</label>
+                <input type="number" name="product_price" id="product_price">
+                <br>
+            </p>
+            <p>
+                <label for="product_quantity">Quantité :</label>
+                <input type="number" name="product_quantity" id="product_quantity">
+                <br>
+            </p>
+        </fieldset>
+    </form>
+
+</body>
+</html>
