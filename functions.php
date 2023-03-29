92b32ca18aa4a24b780a8339056cb75fbee5a98f
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-29 10:57:00 +0200

    (functions) created function getMessages

--- functions.php
+++ functions.php
@@ -1,14 +1,17 @@
 <?php
-
 session_start();
 
 function totalQtt() {
+    
     $result = 0;
     if (!empty($_SESSION['products'])) {
+
         foreach($_SESSION['products'] as $index => $product) {
             $result += $product['qtt'];
         }
-        return $result;
+
+    return $result;
+
     }
 }
 
@@ -18,6 +21,7 @@ function delete($id) {
         unset($_SESSION['products']);
         var_dump("pas good");
     }
+
     else {
         unset($_SESSION['products'][$id]);
         var_dump("good");
@@ -25,20 +29,33 @@ function delete($id) {
 }
 
 function decreaseQtt($id) {
+
     if($_SESSION['products'][$id]['qtt'] > 1) {
         $_SESSION['products'][$id]['qtt'] --;
         refreshTotal($id);
+
     } else {
         delete($id);
     }
 }
 
 function addQtt($id) {
+
     $_SESSION['products'][$id]['qtt'] ++;
     refreshTotal($id);
 }
 
 function refreshTotal($id) {
+
     $_SESSION['products'][$id]['total'] =  $_SESSION['products'][$id]['price']* $_SESSION['products'][$id]['qtt'];
-    // $_SESSION['products'][$totalGeneral] = 
+}
+
+function getMessages() {
+    if(isset($_SESSION['message']) && !empty($_SESSION['message'])) {
+        $result = "<h3>" . $_SESSION['message'] . "</h3>";
+        unset($_SESSION['message']);
+
+        return $result;
+    }
+    return false;
 }
