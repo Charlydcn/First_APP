7c3896dd660602fcb7a0832eb40090adec7000cd
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-29 09:00:41 +0200

    (recap) functions remove product/ basket, +/-qtt

--- recap.php
+++ recap.php
@@ -1,14 +1,16 @@
 <?php
 require "functions.php";
-    
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <style> <?php include 'CSS/style.css'; ?> </style>
+    <link rel="stylesheet" href="CSS/style.css"/>
+   
     <title>Récapitulatif des produits</title>
 </head>
 
@@ -20,15 +22,15 @@ require "functions.php";
             <?=totalQtt()?>
         </a>
         <a href="index.php">
-            Ajout d'un produit
+            Accueil
         </a>
     </nav>
 
     <?php 
     
     if(!isset($_SESSION['products']) || empty($_SESSION['products'])) { // SI products[] n'existe pas, ou qu'il est vide, alors
-    }
-    else {
+    echo "<h1>Le panier est vide !</h1>";
+    } else {
         echo "<table id='products_list'>",
                 "<thead>",
                     "<tr>",
@@ -37,28 +39,32 @@ require "functions.php";
                         "<th>Prix</th>",
                         "<th>Quantité</th>",
                         "<th>Total</th>",
-                    "<tr>",
+                        "<th></th>",
+                    "</tr>",
                 "</thead>",
                 "<tbody>";
 
         $totalGeneral = 0;
         foreach($_SESSION['products'] as $index => $product) { // Pour chaque product dans l'index du tableau products,
             echo "<tr>",
-                    "<tr>",
                         "<td>".$index."</td>",
 
                         "<td>".$product['name']."</td>",
 
                         "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
                     
-                        "<td><input type='button' class='decrease_qtt' value='-'></input>",
+                        "<td><a href='traitement.php?action=decreaseQtt&id=".$index."' class='change_qtt'>-</a>",
                         $product['qtt'],
-                        "<input type='button' class='increase_qtt' value='+'></input>",
+                        "<a href='traitement.php?action=addQtt&id=".$index."' class='change_qtt'>+</a></td>",
                         
                         "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
-                         
 
-                        $totalGeneral+= $product['total'];   
+                        "<td><a href='traitement.php?action=deleteProduct&id=".$index."'>
+                            <i class='fa-solid fa-trash'></i>
+                        </a></td>",
+                         
+                        "</tr>"  ;
+                        $totalGeneral+= $product['total'];
         }
         echo "<tfoot>
             <tr>",
@@ -67,13 +73,15 @@ require "functions.php";
             "</tr>",
         
             "<tr>",
-                "<td colspan=4>Total général : </td>",
-                "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                "<td colspan=4>Total du panier : </td>",
+                "<td><strong>" . number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
             "</tr>",
-            "</tfoot></tbody>";
+            "</tfoot></tbody>",
 
-        echo"</tbody>",
-            "</table>";
+            "</tbody>",
+            "</table>",
+
+            "<a href='traitement.php?action=emptyBasket'>Vider le panier</a>";
 
     }
 
