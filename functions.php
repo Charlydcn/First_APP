9d661cca3591ca5e16be83a1006d7a6265918d96
Charlydcn <Charly.ducournau@outlook.fr>
2023-03-29 08:56:55 +0200

    added add/decQtt, delete/empty basket functions

--- functions.php
+++ functions.php
@@ -2,18 +2,43 @@
 
 session_start();
 
-function totalQtt(){
+function totalQtt() {
     $result = 0;
-    foreach($_SESSION['products'] as $index => $product) {
-        $result += $product['qtt'];
+    if (!empty($_SESSION['products'])) {
+        foreach($_SESSION['products'] as $index => $product) {
+            $result += $product['qtt'];
+        }
+        return $result;
     }
-    return $result;
 }
 
-function addQtt($number) {
-    $_SESSION($product) + $number;
+function delete($id) {
+   
+    if ($id == null) {
+        unset($_SESSION['products']);
+        var_dump("pas good");
+    }
+    else {
+        unset($_SESSION['products'][$id]);
+        var_dump("good");
+    }
 }
 
-// function removeQtt() {
-//     $product['qtt'] - 1;
-// }
+function decreaseQtt($id) {
+    if($_SESSION['products'][$id]['qtt'] > 1) {
+        $_SESSION['products'][$id]['qtt'] --;
+        refreshTotal($id);
+    } else {
+        delete($id);
+    }
+}
+
+function addQtt($id) {
+    $_SESSION['products'][$id]['qtt'] ++;
+    refreshTotal($id);
+}
+
+function refreshTotal($id) {
+    $_SESSION['products'][$id]['total'] =  $_SESSION['products'][$id]['price']* $_SESSION['products'][$id]['qtt'];
+    // $_SESSION['products'][$totalGeneral] = 
+}
